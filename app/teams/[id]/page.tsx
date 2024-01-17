2f0e2ef9757388e72630be2a353c68bc36c44950
jonathanchuangcodes <jchuangsa@gmail.com>
2024-01-17 18:30:01 -0500

    add teams route and interface

---
+++ app/teams/[id]/page.tsx
@@ -0,0 +1,13 @@
+import football from "@/api/football";
+
+export default async function CompetitionPage({ params }: { params: { slug: string, id: 39 } }) {
+    console.log(params.id);
+    let data = await football.get("/teams", { params: { id: params.id } }).then(({ data }) => {
+        return data.response;
+    });
+    return (
+        <div>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    );
+}
